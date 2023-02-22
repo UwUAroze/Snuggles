e0892af968c87f3d20d83b1126bb73c308e460c8
UwUAroze <admin@aroze.me>
2023-02-22 17:42:00 -0500

    Added a little link to github repo

--- src/main/kotlin/me/aroze/snuggles/commands/impl/generic/GlobalStatsCommand.kt
+++ src/main/kotlin/me/aroze/snuggles/commands/impl/generic/GlobalStatsCommand.kt
@@ -3,7 +3,9 @@ package me.aroze.snuggles.commands.impl.generic
 import jda
 import me.aroze.snuggles.commands.BaseCommand
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.Emoji
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.interactions.components.Button
 
 object GlobalStatsCommand : BaseCommand("globalstats", "Some fun little global stats about Snuggles") {
 
@@ -14,8 +16,14 @@ object GlobalStatsCommand : BaseCommand("globalstats", "Some fun little global s
         eb.setDescription("Here's some fun little global stats about Snuggles!")
         eb.setColor(0xFFBAD3)
         eb.addField("Server count", jda.guilds.size.toString(), true)
+        eb.setFooter("Snuggles • Made with ♥ by Aroze#0001")
 
-        event.replyEmbeds(eb.build()).queue()
+        event.replyEmbeds(eb.build())
+            .addActionRow(
+                Button.link("https://github.com/UwUAroze/Snuggles", "I'm open sourced!")
+                    .withEmoji(Emoji.fromMarkdown("<:github:1077388578511917167>")),
+            )
+            .queue()
 
     }
 
