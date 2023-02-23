b22afb46ed4bb1f90881b25c23b8683d00bafc2a
UwUAroze <admin@aroze.me>
2023-02-23 17:54:30 -0500

    Changed needed for DevCommand to function, along with better initialisation of bot

--- src/main/kotlin/me/aroze/snuggles/Snuggles.kt
+++ src/main/kotlin/me/aroze/snuggles/Snuggles.kt
@@ -1,14 +1,21 @@
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import me.aroze.snuggles.commands.BaseCommand
+import me.aroze.snuggles.commands.DevCommand
 import me.aroze.snuggles.commands.feelings.RegisterFeelings.feelings
 import me.aroze.snuggles.commands.feelings.RegisterFeelings.registerFeelings
 import me.aroze.snuggles.config.ConfigLoader
 import me.aroze.snuggles.database.Database
 import me.aroze.snuggles.initialisation.Login.login
 import me.aroze.snuggles.models.BotStats
+import me.aroze.snuggles.utils.safeConstruct
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
 import org.reflections.Reflections
 import java.util.Timer
 import kotlin.collections.ArrayList
@@ -24,16 +31,17 @@ fun main() = runBlocking {
     Database.connect()
 
     login()
+    instance.awaitReady()
     println(instance.selfUser.asTag)
 
     registerFeelings()
 
     val commands = Reflections("me.aroze.snuggles.commands.impl")
         .getSubTypesOf(BaseCommand::class.java)
-        .map { it.getDeclaredConstructor().newInstance() }
+        .mapNotNull { it.safeConstruct() }
 
     registerCommands(
-        *commands.toTypedArray(),
+        *commands.filter {!it.devOnly}.toTypedArray(),
         *feelings.toTypedArray()
     )
 
@@ -65,4 +73,26 @@ private fun registerCommands(vararg commands: BaseCommand) {
         queued.add(command.build)
     }
     instance.updateCommands().addCommands(queued).queue()
+}
+
+private fun registerDevCommands(guild: Guild, vararg commands: BaseCommand) {
+    val queued: MutableList<CommandData> = ArrayList()
+
+    for (command in commands) {
+        instance.addEventListener(command)
+        if (command !is DevCommand) queued.add(command.build)
+        else {
+
+            val commandName = command.info[0]
+            val existing = queued.find { it.name == commandName }
+            if (existing == null) queued.add(CommandData(commandName, "Development Command"))
+
+            queued.find { it.name == commandName }?.addSubcommands(SubcommandData(
+                command.info[1],
+                command.description
+            ).addOptions(*command.optionData.toTypedArray()))
+        }
+    }
+
+    guild.updateCommands().addCommands(queued).queue()
 }
--- src/main/kotlin/me/aroze/snuggles/commands/BaseCommand.kt
+++ src/main/kotlin/me/aroze/snuggles/commands/BaseCommand.kt
@@ -3,31 +3,43 @@ package me.aroze.snuggles.commands
 import me.aroze.snuggles.database.Database
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.Command.Subcommand
 import net.dv8tion.jda.api.interactions.commands.OptionMapping
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 abstract class BaseCommand(private val name: String, private val description: String) : ListenerAdapter() {
     protected val options = ArrayList<Option>()
-    protected val guildOnly = false
     protected var silent = false
+    open val guildOnly = true
+    open val devOnly = false
     open val defaultSilent = false
 
     override fun onSlashCommand(event: SlashCommandEvent) {
-        if (event.name == name) {
+        if (event.name == name.split(" ")[0]) {
             if (guildOnly && event.guild == null) {
                 // TODO: Send error message to user
                 return
             }
+
             Database.botStats.totalExecutions++
             silent = event.getOption("silent")?.asBoolean ?: defaultSilent
             onExecute(event)
         }
     }
 
+    val optionData: List<OptionData>
+        get() {
+            return options.map {
+                OptionData(it.type, it.name, it.description, it.required)
+            }
+        }
+
     val build: CommandData
         get() {
-            val command = CommandData(name, description)
+            val command = CommandData(name.split(" ")[0], description)
             for (option in options) {
                 command.addOption(
                     option.type,
