f4f7b7b5f00d0dc8e949dded56b10d07655d0b08
UwUAroze <admin@aroze.me>
2023-02-23 19:14:36 -0500

    Redesigned /dev guilds

--- src/main/kotlin/me/aroze/snuggles/commands/impl/dev/FetchGuildsCommand.kt
+++ src/main/kotlin/me/aroze/snuggles/commands/impl/dev/FetchGuildsCommand.kt
@@ -2,17 +2,28 @@ package me.aroze.snuggles.commands.impl.dev
 
 import instance
 import me.aroze.snuggles.commands.DevCommand
+import me.aroze.snuggles.utils.BarStyle
+import me.aroze.snuggles.utils.FancyEmbed
+import me.aroze.snuggles.utils.bar
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 
 class FetchGuildsCommand: DevCommand("dev guilds", "Fetch all guilds") {
     override fun onSubCommand(event: SlashCommandEvent) {
-        val sb = StringBuilder()
-        instance.guilds.forEach { guild ->
-            sb.append("\n\n**${guild.name}** (`${guild.id}`)")
-                .append("\nOwner: ${instance.getUserById(guild.ownerId)?.asTag ?: "Unknown"}")
-                .append("\nMembers: ${guild.memberCount}")
+
+        val eb = FancyEmbed()
+            .setTitle("Snuggles is in `${instance.guilds.size}` guilds!")
+            .setDescription("Here's some info about them <:eyesZoom:953368332789252166>")
+
+        for ((i, guild) in instance.guilds.withIndex()) {
+            eb.addField("\u200B\n\u200B${guild.name} `${guild.id}`",
+                "Is owned by ${instance.getUserById(guild.ownerId)?.asTag ?: "Unknown"} with ${guild.memberCount} members" +
+                "\nGuild was created: <t:${guild.timeCreated.toEpochSecond()}:R> and Snuggles joined: <t:${guild.selfMember.timeJoined.toEpochSecond()}:R>",
+            false)
         }
 
-        event.reply(sb.toString()).setEphemeral(silent).queue()
+        event.replyEmbeds(eb.build())
+            .bar(BarStyle.PINK)
+            .setEphemeral(silent)
+            .queue()
     }
 }
