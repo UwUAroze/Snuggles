61c1c1fba2fc6c82411ecdcee4a23418c86a988e
UwUAroze <admin@aroze.me>
2023-03-04 22:03:46 +0000

    Made entity selection event async

--- src/main/kotlin/me/aroze/snuggles/listeners/SelectionListener.kt
+++ src/main/kotlin/me/aroze/snuggles/listeners/SelectionListener.kt
@@ -8,20 +8,23 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 object SelectionListener : ListenerAdapter() {
 
-    override fun onEntitySelectInteraction(event: EntitySelectInteractionEvent) {
-        if (event.componentId == "counting-channels") {
-            val channel = event.mentions.channels.first()
-            CountData.create(channel.id, event.guild!!.id)
-            println("Counting channel set to ${channel.name}")
+    override fun onEntitySelectInteraction(event: EntitySelectInteractionEvent) = runBlocking {
+        launch {
+            if (event.componentId == "counting-channels") {
+                val channel = event.mentions.channels.first()
+                CountData.create(channel.id, event.guild!!.id)
+                println("Counting channel set to ${channel.name}")
 
-            val eb = FancyEmbed()
-                .setDescription("This server's counting channel has been set to ${channel.asMention}")
+                val eb = FancyEmbed()
+                    .setDescription("This server's counting channel has been set to ${channel.asMention}")
 
-            event.replyEmbeds(eb.build())
-                .setEphemeral(true)
-                .queue()
+                event.replyEmbeds(eb.build())
+                    .setEphemeral(true)
+                    .queue()
 
+            }
         }
+        Unit
     }
 
 }
