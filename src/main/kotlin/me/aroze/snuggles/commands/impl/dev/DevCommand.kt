ce659a0389fe21f00daefde4601872a72e81dff5
UwUAroze <admin@aroze.me>
2023-03-04 22:02:54 +0000

    Fixed guild owners in /dev guilds

--- src/main/kotlin/me/aroze/snuggles/commands/impl/dev/DevCommand.kt
+++ src/main/kotlin/me/aroze/snuggles/commands/impl/dev/DevCommand.kt
@@ -1,6 +1,8 @@
 package me.aroze.snuggles.commands.impl.dev
 
 import instance
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import me.aroze.snuggles.commands.handler.Command
 import me.aroze.snuggles.commands.handler.CommandEvent
 import me.aroze.snuggles.utils.BarStyle
@@ -23,21 +25,27 @@ class DevCommand {
     }
 
     @Command(description = "Fetch all guilds")
-    fun guilds(event: CommandEvent) {
+    fun guilds(event: CommandEvent) = runBlocking {
         val eb = FancyEmbed()
             .setTitle("Snuggles is in `${instance.guilds.size}` guilds!")
             .setDescription("Here's some info about them <:eyesZoom:953368332789252166>")
 
-        for ((i, guild) in instance.guilds.withIndex()) {
-            eb.addField("\u200B\n\u200B${guild.name} `${guild.id}`",
-                "Is owned by ${instance.getUserById(guild.ownerId)?.asTag ?: "Unknown"} with ${guild.memberCount} members" +
-                    "\nGuild was created: <t:${guild.timeCreated.toEpochSecond()}:R> and Snuggles joined: <t:${guild.selfMember.timeJoined.toEpochSecond()}:R>",
-                false)
-        }
+        launch {
+            for ((i, guild) in instance.guilds.withIndex()) {
+                val owner = guild.retrieveMemberById(guild.ownerId).complete()
+                eb.addField(
+                    "\u200B\n\u200B${guild.name} `${guild.id}`",
+                    "Is owned by ${owner.user.asTag ?: "Unknown"} with ${guild.memberCount} members" +
+                        "\nGuild was created: <t:${guild.timeCreated.toEpochSecond()}:R> and Snuggles joined: <t:${guild.selfMember.timeJoined.toEpochSecond()}:R>",
+                    false
+                )
 
-        event.message.replyEmbeds(eb.build())
-            .bar(BarStyle.PINK)
-            .setEphemeral(event.silent)
-            .queue()
+            }
+
+            event.message.replyEmbeds(eb.build())
+                .bar(BarStyle.PINK)
+                .setEphemeral(event.silent)
+                .queue()
+        }
     }
 }
