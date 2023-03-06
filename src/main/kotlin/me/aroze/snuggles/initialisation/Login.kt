917d5d6b0f3ca31f5a8c28061d863ae3f0c7bcd8
UwUAroze <admin@aroze.me>
2023-03-06 02:48:34 +0000

    Changed intents + cache policy

--- src/main/kotlin/me/aroze/snuggles/initialisation/Login.kt
+++ src/main/kotlin/me/aroze/snuggles/initialisation/Login.kt
@@ -5,6 +5,7 @@ import me.aroze.snuggles.config.ConfigLoader
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.utils.messages.MessageRequest
 
 object Login {
@@ -12,7 +13,8 @@ object Login {
     fun login() {
         instance = JDABuilder
             .createDefault(ConfigLoader.config.getString("authentication.token"))
-            .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+            .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS)
+            .setMemberCachePolicy(MemberCachePolicy.ALL)
             .build()
 
         MessageRequest.setDefaultMentions(listOf(Message.MentionType.USER, Message.MentionType.CHANNEL, Message.MentionType.EMOJI))
