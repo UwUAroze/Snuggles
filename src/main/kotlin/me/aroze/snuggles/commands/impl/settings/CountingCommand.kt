0cebf22a95ad190d40369b1bcd4d7becaeafd6ee
UwUAroze <admin@aroze.me>
2023-03-07 00:22:17 +0000

    Removed outdated code

--- src/main/kotlin/me/aroze/snuggles/commands/impl/settings/CountingCommand.kt
+++ src/main/kotlin/me/aroze/snuggles/commands/impl/settings/CountingCommand.kt
@@ -26,61 +26,28 @@ class CountingCommand {
     fun settings(event: CommandEvent) = runBlocking {
         launch {
             val count = CountData.getByGuild(event.message.guild!!.id)
-            val replyData = getCountingUI(count)
+            val channel = count?.id?.let { instance.getGuildChannelById(it) } as? TextChannel
 
-            event.message.replyEmbeds(replyData.embed.build())
-                .addActionRow(replyData.channelSelection)
-                .addActionRow(replyData.settingsSelection)
-                .setEphemeral(true)
-                .queue()
-        }
-    }
+            ChannelMenu("Counting") {
 
-    companion object {
-        @Ignore
-        fun getCountingUI(count: CountData?) : CountingSettingData {
+                println(it.channel?.id)
+                println(it.channel?.name)
+                println(it.channel?.guild?.id)
+                println(it.settings)
 
-            val channelSelection = EntitySelectMenu.create("counting-channels", EntitySelectMenu.SelectTarget.CHANNEL)
-                .setChannelTypes(ChannelType.TEXT)
-                .setPlaceholder("Select a channel for counting to take place in.")
-                .setRequiredRange(0, 1)
+                if (it.channel == null) {
+                    count?.disabled = true
+                } else CountData.create(it.channel.id, event.message.guild!!.id)
 
-            val settingsSelection = StringSelectMenu.create("counting-settings")
-                .setPlaceholder("Counting settings (select a channel first!)")
-                .setRequiredRange(0, 25)
-                .setDisabled(true)
-                .addOption("Consecutive counting", "consecutive-counting", "Allows for the same user to count multiple times in a row.")
-                .addOption("Allow speaking", "allow-speaking", "Allows for users to speak in the counting channel.")
+                count?.allowConsecutiveUsers = it.settings.contains("consecutive_counting")
+                count?.allowTalking = it.settings.contains("allow_speaking")
 
-            if (count != null && !count.disabled) {
-                val channel = instance.getGuildChannelById(count.id)
-                settingsSelection
-                    .setDefaultOptions(count.getSelectedOptions())
-                    .setDisabled(false)
-                    .placeholder = "Toggle settings for the counting module."
-                channelSelection
-                    .placeholder = "Select a channel. Current: #${channel?.name ?: "deleted channel"}"
             }
-
-            val eb = FancyEmbed()
-                .setTitle("Counting Settings <:flushed_cool:1081364373295087686>")
-                .setDescription(
-                    "The menu below is *very self explanatory*. But if ur stupid ass needs help, I got u <3\n\n" +
-                    "The thingy that says `select a channel` is to select a channel. If the server has " +
-                    "a channel selected, the other thingy that says `toggle settings` becomes usable, " +
-                    "and allows you to, you guessed it, toggle settings.\n\n" +
-                    "...You're fucking welcome"
-                )
-
-            return CountingSettingData(eb, channelSelection.build(), settingsSelection.build())
+                .addOption(MenuOption("Consecutive counting", "Allow users to count consecutively."))
+                .addOption(MenuOption("Allow speaking", "Allow users to speak in the counting channel."))
+                .setChannel(channel)
+                .setSelectedOptions(count?.getSelectedOptions())
+                .send(event.message)
         }
     }
-
-    @Ignore
-    data class CountingSettingData(
-        val embed: EmbedBuilder,
-        val channelSelection: EntitySelectMenu,
-        val settingsSelection: StringSelectMenu
-    )
-
 }
