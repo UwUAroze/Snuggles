9494dce61796e46c4173559a9a69031af7af1d9e
UwUAroze <admin@aroze.me>
2023-03-08 15:49:36 +0000

    Fixed some issues with querying

--- src/main/kotlin/me/aroze/snuggles/models/UserData.kt
+++ src/main/kotlin/me/aroze/snuggles/models/UserData.kt
@@ -18,7 +18,7 @@ data class UserData(
     fun save() {
         val collection = database.getCollection<UserData>()
         collection.findOneAndReplace(
-            ::id eq this.id,
+            and(::id eq this.id, ::guild eq this.guild),
             this,
             FindOneAndReplaceOptions().upsert(true)
         )
@@ -31,12 +31,12 @@ data class UserData(
         fun get(id: String, guild: String): UserData? {
             return instances.firstOrNull { it.id == id && it.guild == guild } ?: let {
                 val collection = database.getCollection<UserData>()
-                collection.findOne(UserData::id eq id, UserData::guild eq guild)?.also { UserData.instances.add(it) }
+                collection.findOne(UserData::id eq id, UserData::guild eq guild)?.also { instances.add(it) }
             }
         }
 
         fun create(id: String, guild: String): UserData {
-            val data = UserData.get(id, guild) ?: UserData(id, guild)
+            val data = get(id, guild) ?: UserData(id, guild)
             instances.removeIf { it.id == id && it.guild == guild }
             instances.add(data)
             return data
