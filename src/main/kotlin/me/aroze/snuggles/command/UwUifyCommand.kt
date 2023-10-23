84d6c47294e8d24ad99148b5fd0553a5ec6ff34c
UwUAroze <admin@aroze.me>
2023-10-23 23:49:30 +0100

    feat: /uwuify

---
+++ src/main/kotlin/me/aroze/snuggles/command/UwUifyCommand.kt
@@ -0,0 +1,22 @@
+package me.aroze.snuggles.command
+
+import me.aroze.arozeutils.minecraft.generic.text.uwuify
+import me.santio.coffee.common.annotations.Command
+import me.santio.coffee.common.annotations.Optional
+import me.santio.coffee.jda.annotations.Description
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+@Command("uwuify")
+@Description("UwUify a message")
+class UwUifyCommand {
+
+    fun uwuify(
+        event: SlashCommandInteractionEvent,
+        @Description("The message to UwUify") message: String,
+        @Optional @Description("Should this only show to you? (it won't by default!)") silent: Boolean?
+    ) {
+        // uwu ;3 -santio
+        event.reply(uwuify(message)).setEphemeral(silent ?: false).queue()
+    }
+
+}
