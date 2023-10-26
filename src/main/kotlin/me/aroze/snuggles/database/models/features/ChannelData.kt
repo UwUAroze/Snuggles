32eb899e8c74c9dccae058b034729a55ff28d49e
UwUAroze <admin@aroze.me>
2023-10-26 02:40:13 +0100

    feat: getLoggingData()

--- src/main/kotlin/me/aroze/snuggles/database/models/features/ChannelData.kt
+++ src/main/kotlin/me/aroze/snuggles/database/models/features/ChannelData.kt
@@ -2,10 +2,8 @@ package me.aroze.snuggles.database.models.features
 
 import com.mongodb.client.model.Filters
 import me.aroze.snuggles.database.Database
-import me.aroze.snuggles.database.models.SnugglyStats
 import me.aroze.snuggles.database.models.features.generic.impl.LoggingData
 import me.aroze.snuggles.initialisation.BotLoader.snuggles
-import org.bson.BsonUndefined
 
 data class ChannelData(
     val channel: String,
@@ -13,11 +11,23 @@ data class ChannelData(
 ) {
 
     companion object {
-        fun getByGuild(id: String): List<ChannelData> {
+        fun getByGuild(guild: String): List<ChannelData> {
             val collection = Database.database.getCollection<ChannelData>("ChannelData")
-            val channels = snuggles.getGuildById(id)?.textChannels?.map { it.id } ?: listOf()
+            val channels = snuggles.getGuildById(guild)?.textChannels?.map { it.id } ?: listOf()
             return collection.find(Filters.`in`("channel", channels)).toList()
         }
+
+        fun getLoggingData(guild: String) = getSpecificData(guild, "logging")?.logging
+
+        private fun getSpecificData(guild: String, type: String): ChannelData? {
+            val collection = Database.database.getCollection<ChannelData>("ChannelData")
+            val channels = snuggles.getGuildById(guild)?.textChannels?.map { it.id } ?: listOf()
+            return collection.find(Filters.and(
+                Filters.`in`("channel", channels),
+                Filters.exists(type)
+            )).toList().firstOrNull()
+        }
+
     }
 
 }
