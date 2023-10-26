82bb3b8bfd4b2221654f3e0e6cabb16d942cfe50
UwUAroze <admin@aroze.me>
2023-10-26 02:54:30 +0100

    feat: errorGuildOnly() response

---
+++ src/main/kotlin/me/aroze/snuggles/util/Responses.kt
@@ -0,0 +1,22 @@
+package me.aroze.snuggles.util
+
+import me.aroze.snuggles.util.type.FancyEmbed
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+object Responses {
+
+    fun errorGuildOnly(event: SlashCommandInteractionEvent) {
+        val eb = FancyEmbed()
+            .addField("Awwwwhh.",
+                """
+                This command is only supported in guilds ;c. If you really want support for it in dms, well you're weird, but maybe shout at a dev!
+                ~~Then again, we probably won't care.~~ Love you <3
+                """.trimIndent(), false)
+
+        event.replyEmbeds(eb.build())
+            .bar(BarStyle.ERROR)
+            .setEphemeral(true)
+            .queue()
+    }
+
+}
