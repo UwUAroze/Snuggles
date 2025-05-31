6bedb9242c94a3bbee419a3e9867929c6096ca82
Aroze <admin@aroze.me>
2025-05-31 20:03:30 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "me.aroze"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/me/aroze/snuggles/Snuggles.kt
@@ -0,0 +1,10 @@
+package me.aroze.snuggles
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
